e3d0d99d5348c82ea50d6ef02c3f0a91ec7cf285
miracle boniface <udohmiracle23@gmail.com>
2026-05-28 20:15:14 +0100

    repo updated

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+import bodyParser from 'body-parser';
+import { ValidationPipe } from '@nestjs/common';
+config()
+
+const port = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(
+    bodyParser.urlencoded({
+      limit: '10mb',
+      extended: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://waitlist.nebourhood.org'], // allow requests from your frontend
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,               // allow cookies/auth headers if needed
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // strips properties not in the DTO
+      forbidNonWhitelisted: true, // throws error if extra properties
+      transform: true, // transforms plain JSON into class instances
+    }),
+  );
+
+  await app.listen(port);
 }
 bootstrap();
