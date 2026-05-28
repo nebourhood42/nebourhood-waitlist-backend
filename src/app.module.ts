3a4d4b2dcea73f456035bfcb380033caa99be260
miracle boniface <udohmiracle23@gmail.com>
2026-05-28 23:38:25 +0100

    repo updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { configDotenv } from 'dotenv';
-configDotenv()
+import { config } from 'dotenv';
+config()
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(process.env.MONGO_DB),
+    MongooseModule.forRoot(process.env.MONGO_DB!),
     PassportModule,
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET!,
       signOptions: { expiresIn: '30d' },
     }),
-    // AuthModule
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
